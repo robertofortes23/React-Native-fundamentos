acb125814edab892a913512bac797d779fd4ca89
Roberto Fortes <robertbernd23@gmail.com>
2021-07-30 06:50:46 -0300

    Comunicação direta e comunicação indireta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import Contador from './components/Contador'
-import Botao from './components/Botao'
+import PaiIndireta from './components/indireta/Pai'
+import PaiDireta from './components/direta/Pai'
+// import Contador from './components/Contador'
+// import Botao from './components/Botao'
 // import Titulo from './components/Titulo'
 // import Aleatorio from './components/Aleatorio'
 // import MinMax from './components/MinMax'
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <Contador inicial={100} passo={13} />
-        <Contador />
-        <Botao />
+        <PaiIndireta />
+        <PaiDireta />
     </SafeAreaView>
 )
 
