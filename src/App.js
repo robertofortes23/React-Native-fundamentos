eed095d2dea2e1e11f045c621806e7f7f3e30efd
Roberto Fortes <robertbernd23@gmail.com>
2021-07-30 06:36:46 -0300

    Componente funcional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
-export default class App extends React.Component {
-	render() {
-		return (
-			<SafeAreaView style={style.App}>
 
-			</SafeAreaView>
-		)
-	}
-}
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	}
+export default () => (
+    <SafeAreaView style={style.App}>
+
+    </SafeAreaView>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+    }
 })
