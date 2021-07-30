03b053206dee5f6754d98c68974a7eccad709525
Roberto Fortes <robertbernd23@gmail.com>
2021-07-30 06:58:36 -0300

    Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import Diferenciar from './components/Diferenciar'
+import ParImpar from './components/ParImpar'
+// import Diferenciar from './components/Diferenciar'
 // import ContadorV2 from './components/contador/ContadorV2'
 // import Pai from './components/indireta/Pai'
 // import Pai from './components/direta/Pai'
@@ -10,10 +11,12 @@ import Diferenciar from './components/Diferenciar'
 // import Titulo from './components/Titulo'
 // import Aleatorio from './components/Aleatorio'
 // import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <Diferenciar />
+        <ParImpar num={3}/>
     </SafeAreaView>
 )
 
